8e76ffdaeb67451298abe9b6b0045b235559138d
arthask <arthask@gmail.com>
2024-10-07 23:23:08 +0800

    修改文件starter

--- file-spring-boot-starter/src/main/java/com/example/filespringbootstarter/core/factory/FileClientFactoryImpl.java
+++ file-spring-boot-starter/src/main/java/com/example/filespringbootstarter/core/factory/FileClientFactoryImpl.java
@@ -6,6 +6,7 @@ import com.example.filespringbootstarter.core.client.AbstractFileClient;
 import com.example.filespringbootstarter.core.client.FileClient;
 import com.example.filespringbootstarter.enums.FileStorageEnum;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.Assert;
 
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
@@ -25,14 +26,29 @@ public class FileClientFactoryImpl implements FileClientFactory {
      */
     private final ConcurrentMap<FileStorageEnum, FileClient> clients = new ConcurrentHashMap<>();
 
+    private final FileClientConfig localFileClientConfig;
+
+    private final FileClientConfig s3FileClientConfig;
+
+
     public FileClientFactoryImpl(FileClientConfig localFileClientConfig, FileClientConfig s3FileClientConfig) {
-        clients.put(FileStorageEnum.LOCAL, createFileClient(FileStorageEnum.LOCAL, localFileClientConfig));
-        clients.put(FileStorageEnum.S3, createFileClient(FileStorageEnum.S3, s3FileClientConfig));
+        this.localFileClientConfig = localFileClientConfig;
+        this.s3FileClientConfig = s3FileClientConfig;
     }
 
     @Override
     public FileClient getFileClient(FileStorageEnum fileStorageEnum) {
         fileStorageEnum = Optional.ofNullable(fileStorageEnum).orElse(FileStorageEnum.LOCAL);
+        if (!clients.containsKey(fileStorageEnum)) {
+            if (FileStorageEnum.LOCAL.equals(fileStorageEnum)) {
+                Assert.notNull(localFileClientConfig, "请正确配置localFileClientConfig");
+                clients.put(FileStorageEnum.LOCAL, createFileClient(FileStorageEnum.LOCAL, localFileClientConfig));
+            }
+            if (FileStorageEnum.S3.equals(fileStorageEnum)) {
+                Assert.notNull(s3FileClientConfig, "请正确配置s3FileClientConfig");
+                clients.put(FileStorageEnum.S3, createFileClient(FileStorageEnum.S3, s3FileClientConfig));
+            }
+        }
         return clients.get(fileStorageEnum);
     }
 
